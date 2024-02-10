2d25e678c06b37c86e26117e8cb9d1fd01afa06d
Nahid4306053 <131442604+Nahid4306053@users.noreply.github.com>
2024-02-10 19:47:20 +0600

    add custom marker

--- src/components/UserDetails/Map.jsx
+++ src/components/UserDetails/Map.jsx
@@ -1,15 +1,25 @@
 /* eslint-disable react/prop-types */
+import L from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-const Map = ({lat, lng, address}) => {
- 
+// Import your custom marker icon image
+import customMarkerIcon from '/marker-icon.png';
+
+// Define custom icon
+const customIcon = L.icon({
+  iconUrl: customMarkerIcon,
+  iconSize: [28, 40], // Adjust the size of the icon as per your requirement
+  iconAnchor: [16, 32], // Adjust the anchor point if needed
+});
+
+const Map = ({ lat, lng, address }) => {
   return (
     <MapContainer center={[lat, lng]} zoom={13} style={{ height: '400px', width: '100%' }}>
       <TileLayer 
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[lat, lng]}>
+      <Marker position={[lat, lng]} icon={customIcon}>
         <Popup>
           {address}
         </Popup>
